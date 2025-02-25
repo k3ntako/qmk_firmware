9ae5e77f746fa562914db4308ae11b2332918c49
Kentaro Kaneki <kentarokaneki@gmail.com>
2025-02-25 01:48:44 -0500

    Password and lock screen

---
+++ keyboards/drop/thekey/v2/keymaps/K3ntako/keymap.c
@@ -0,0 +1,36 @@
+// Copyright 2023 Massdrop, Inc.
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_LCTL, KC_ESCAPE, UG_TOGG)};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_LCTL:
+            if (record->event.pressed) {
+                SEND_STRING("1234\n");
+            }
+            return false;
+        case KC_ESCAPE:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                register_code(KC_LCMD);
+                register_code(KC_Q);
+                send_keyboard_report();
+                unregister_code(KC_Q);
+                unregister_code(KC_LCMD);
+                unregister_code(KC_LCTL);
+                send_keyboard_report();
+
+                wait_ms(300);
+
+                register_code(KC_ESC);
+                send_keyboard_report();
+                unregister_code(KC_ESC);
+                send_keyboard_report();
+            }
+            return false;
+    }
+
+    return true;
+}
