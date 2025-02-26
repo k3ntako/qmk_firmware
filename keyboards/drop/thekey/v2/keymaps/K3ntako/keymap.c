28c0a5391f96c4be60a4c51fdfa50f88287e0484
Kentaro Kaneki <kentarokaneki@gmail.com>
2025-02-26 00:35:54 -0500

    Remove layers and add second escape

--- keyboards/drop/thekey/v2/keymaps/K3ntako/keymap.c
+++ keyboards/drop/thekey/v2/keymaps/K3ntako/keymap.c
@@ -2,16 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes {
-    LAYER_SWITCH = SAFE_RANGE,
-};
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_LCTL, KC_ESCAPE, UG_NEXT),
-    [0] = LAYOUT(LAYER_SWITCH, KC_LCTL, KC_ESCAPE),
-    [1] = LAYOUT(LAYER_SWITCH, UG_TOGG, UG_NEXT),
-    [2] = LAYOUT(LAYER_SWITCH, UG_VALD, UG_VALU),
-};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_LCTL, KC_ESCAPE, UG_TOGG)};
 
 uint16_t layer = 0;
 
@@ -39,19 +30,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 send_keyboard_report();
                 unregister_code(KC_ESC);
                 send_keyboard_report();
-            }
-            return false;
-        case LAYER_SWITCH:
-            if (record->event.pressed) {
-                if (layer > 0) {
-                    layer_off(layer);
-                }
 
-                layer = (layer + 1) % 5;
+                wait_ms(300);
 
-                if (layer > 0) {
-                    layer_on(layer);
-                }
+                register_code(KC_ESC);
+                send_keyboard_report();
+                unregister_code(KC_ESC);
+                send_keyboard_report();
             }
             return false;
     }
@@ -60,5 +45,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void keyboard_post_init_user(void) {
-    rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL + 5); // Set to static color mode
+    rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL + 5);
 }
